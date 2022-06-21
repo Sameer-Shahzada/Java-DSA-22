c70e33049c42fc601eadf59017f4b0646e9ba8f0
Shahzada <md.shahzada1606@gmail.com>
2022-06-21 12:21:15 +0530

    Java + DSA start

---
+++ JavaBasic1_PractiseProblemSet/Product.java
@@ -0,0 +1,28 @@
+
+//-> Write a program that takes two numbers as input and display the product of two numbers.
+
+package JavaBasic1_PractiseProblemSet;
+import java.util.Scanner;
+
+class Product
+{
+    public static void main(String[] args) {
+        // define data fields 
+        int num1 , num2;
+
+        System.out.println("Please enter num1 & num2");
+
+        Scanner sc = new Scanner(System.in);
+        num1 = sc.nextInt();
+
+        Scanner sc2 = new Scanner(System.in);
+        num2 = sc2 .nextInt();
+
+        sc.close();
+        sc2.close();
+
+        int product = num1 * num2;
+
+        System.out.println("Product of two digit is - " + num1 + "*" + num2 + " = " + product);
+    }
+}
