884a230b738dec0c70e0c8dabff2c30c3f4fbe6f
Shahzada <md.shahzada1606@gmail.com>
2022-06-25 00:14:59 +0530

    Practise Problem Set 2 updated

--- JavaBasic2_PractiseProblemSet/Table.java
+++ JavaBasic2_PractiseProblemSet/Table.java
@@ -1,12 +1,5 @@
-/*
  
-//-> Write a java program that takes a number as input and print its multiplication table upto
-10.
-
- */
-
-
-package JavaBasic2_PractiseProblemSet;
+//-> Write a java program that takes a number as input and print its multiplication table upto 10.
 
 import java.util.Scanner;
 
@@ -14,17 +7,19 @@ class Table
 {
     public static void main(String[] args) {
         
+        // define data fields
         int num;
 
         System.out.println("Please enter any number to print the table");
 
+        // to take input from user 
         Scanner sc = new Scanner(System.in);
         num = sc.nextInt();
-       
+
         for(int i = 1; i < 11; i++)
         {
-            System.out.println(num * i);
+            System.out.println("\n" + num  + " * " + i + " = " +  i * num);
         }
 
-    }
+    }    
 }
